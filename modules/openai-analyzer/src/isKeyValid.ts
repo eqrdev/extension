5ca50986ddc54bcea31b21018263e88289641b65
Péter Viola <info@violapeter.hu>
2023-08-20 16:42:30 +0200

    feat: early return in isKeyValid, if not match with a regex

--- modules/openai-analyzer/src/isKeyValid.ts
+++ modules/openai-analyzer/src/isKeyValid.ts
@@ -1,4 +1,11 @@
 import { OpenAIClient } from './client'
 
-export const isKeyValid = async (apiKey: string): Promise<boolean> =>
-  (await new OpenAIClient({ apiKey }).get('usage')).status === 200
+export const isKeyValid = async (apiKey: string): Promise<boolean> => {
+  const apiKeyRegex = /^sk-[A-Za-z0-9]{29}$/
+
+  if (!apiKeyRegex.test(apiKey)) {
+    return false
+  }
+
+  return (await new OpenAIClient({ apiKey }).get('models')).status === 200
+}

896d543fcd1ff0c7557c36a9b633b9c4c9c949cc
Péter Viola <info@violapeter.hu>
2023-08-29 22:15:56 +0200

    fix: the isKeyValid method now checks the api key with the correct regular expression

--- modules/openai-analyzer/src/isKeyValid.ts
+++ modules/openai-analyzer/src/isKeyValid.ts
@@ -1,7 +1,7 @@
 import { OpenAIClient } from './client'
 
 export const isKeyValid = async (apiKey: string): Promise<boolean> => {
-  const apiKeyRegex = /^sk-[A-Za-z0-9]{29}$/
+  const apiKeyRegex = /^sk-[A-Za-z0-9]{48}$/
 
   if (!apiKeyRegex.test(apiKey)) {
     return false

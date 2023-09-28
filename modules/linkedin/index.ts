d9e4782493cefd5ed30c50cd79f6b6fc25904551
Péter Viola <info@violapeter.hu>
2023-09-28 09:51:57 +0200

    refactor(linkedin): expose types of linkedin client

--- modules/linkedin/index.ts
+++ modules/linkedin/index.ts
@@ -1,3 +1,10 @@
-export { Conversation, Message } from './src/types/common/Entities'
 export { LinkedInClient } from './src/LinkedInClient'
 export { Invitation } from './src/types/Invitation'
+export { Invitations } from './src/types/Invitations'
+export { VoyagerInvitations } from './src/types/VoyagerInvitations'
+export * as Entities from './src/types/common/Entities'
+export * as VoyagerEntities from './src/types/common/VoyagerEntities'
+export {
+  ConversationsResponse,
+  ConversationMessagesResponse,
+} from './src/types/Conversation'

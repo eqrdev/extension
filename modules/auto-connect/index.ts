12a48fda134ce25a49a3c32fc04e671fc856bd55
Péter Viola <info@violapeter.hu>
2023-10-25 15:50:55 +0200

    feat(auto-connect): validate all configurations in auto connect

--- modules/auto-connect/index.ts
+++ modules/auto-connect/index.ts
@@ -1,18 +1,26 @@
+import { resolve } from 'path'
 import { AutoConnect } from './src/AutoConnect'
 import { EqualizerLogger } from './src/EqualizerLogger'
 import { PersistentStorage } from './src/PersistentStorage'
 import { PuppeteerBrowserService } from './src/PuppeteerBrowserService'
 import {
+  ConfigurationValidator,
   ConversationEvaluator,
   DateEvaluator,
   InvitationEvaluator,
   OpenAIEvaluator,
   ProfileUrl,
+  DEFAULT_AUTO_REPLY_TEXT,
 } from 'equalizer'
 import { LinkedInService } from './src/LinkedInService'
 import * as process from 'process'
+import { FileStorage } from './src/FileStorage'
+import { EqualizerStoredData } from './src/Types/EqualizerStoredData'
 
-const persistentStorage = new PersistentStorage(Date)
+const fileStorage = new FileStorage<EqualizerStoredData>(
+  resolve(__dirname, './.storage/equalizer.json')
+)
+const persistentStorage = new PersistentStorage(Date, fileStorage)
 const browserService = new PuppeteerBrowserService({
   baseUrl: 'https://www.linkedin.com/',
   cookies: [
@@ -24,26 +32,68 @@ const browserService = new PuppeteerBrowserService({
   ],
   runInContainer: process.env.PUPPETEER_RUN_IN_DOCKER === 'true',
 })
-const openAiEvaluator = new OpenAIEvaluator(process.env.OPENAI_API_KEY)
-const dateEvaluator = new DateEvaluator(Date)
-const invitationEvaluator = new InvitationEvaluator(openAiEvaluator)
-const conversationEvaluator = new ConversationEvaluator(
-  dateEvaluator,
-  openAiEvaluator
-)
-const linkedInService = new LinkedInService(browserService)
 const logger = new EqualizerLogger()
+const dateEvaluator = new DateEvaluator(Date)
+const linkedInService = new LinkedInService(browserService)
 
-const profileUrl = new ProfileUrl(process.env.LI_PROFILE_ID)
-const message = profileUrl.replaceInText(process.env.LI_AUTO_MESSAGE)
+;(async () => {
+  const hasOpenAi = OpenAIEvaluator.isKeyValid(process.env.OPENAI_API_KEY)
 
-const autoConnect = new AutoConnect(
-  persistentStorage,
-  linkedInService,
-  invitationEvaluator,
-  conversationEvaluator,
-  logger,
-  message
-)
+  if (!(await linkedInService.isUserLoggedIn())) {
+    logger.log(
+      "We haven't managed to login to your LinkedIn account with the provided session token. Monitoring hasn't started."
+    )
+    return
+  }
 
-autoConnect.monitorEnquiries().then()
+  if (!hasOpenAi) {
+    logger.log(
+      'OpenAI API key is not valid. We do not evaluate the messages automatically.'
+    )
+  }
+
+  if (
+    !ConfigurationValidator.isEqualizerProfileNameValid(
+      process.env.LI_PROFILE_ID
+    )
+  ) {
+    logger.log(
+      "Provided Equalizer profile name is invalid. Monitoring hasn't started."
+    )
+    return
+  }
+
+  const profileUrl = new ProfileUrl(process.env.LI_PROFILE_ID)
+
+  const isMessageValid = ConfigurationValidator.isMessageValid(
+    process.env.LI_AUTO_MESSAGE
+  )
+  if (!isMessageValid) {
+    logger.log(
+      'Provided auto message is invalid. We provided a default reply for you.'
+    )
+  }
+  const message = profileUrl.replaceInText(
+    isMessageValid ? process.env.LI_AUTO_MESSAGE : DEFAULT_AUTO_REPLY_TEXT
+  )
+
+  const openAiEvaluator = hasOpenAi
+    ? new OpenAIEvaluator(process.env.OPENAI_API_KEY)
+    : undefined
+  const invitationEvaluator = new InvitationEvaluator(openAiEvaluator)
+  const conversationEvaluator = new ConversationEvaluator(
+    dateEvaluator,
+    openAiEvaluator
+  )
+
+  const autoConnect = new AutoConnect(
+    persistentStorage,
+    linkedInService,
+    invitationEvaluator,
+    conversationEvaluator,
+    logger,
+    message
+  )
+
+  await autoConnect.monitorEnquiries()
+})()

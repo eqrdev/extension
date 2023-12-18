35040e9607e90c6668b4483d561a43c78b6e131c
violapeter <info@violapeter.hu>
2023-12-18 21:48:54 +0100

    fix: fix parallel running

--- modules/auto-connect/index.ts
+++ modules/auto-connect/index.ts
@@ -1,110 +1,23 @@
 import { resolve } from 'path'
-import Bottleneck from 'bottleneck'
-import { scheduleJob } from 'node-schedule'
-import { AutoConnect } from './src/AutoConnect'
+import { fork } from 'child_process'
+import { Scheduler } from './src/Scheduler'
 import { EqualizerLogger } from './src/EqualizerLogger'
-import { PersistentStorage } from './src/PersistentStorage'
-import { PuppeteerBrowserService } from './src/PuppeteerBrowserService'
-import {
-  ConfigurationValidator,
-  ConversationEvaluator,
-  DateEvaluator,
-  InvitationEvaluator,
-  OpenAIEvaluator,
-  ProfileUrl,
-  DEFAULT_AUTO_REPLY_TEXT,
-} from 'equalizer'
-import { LinkedInService } from './src/LinkedInService'
-import * as process from 'process'
-import { FileStorage } from './src/FileStorage'
-import { EqualizerStoredData } from './src/Types/EqualizerStoredData'
 
-const fileStorage = new FileStorage<EqualizerStoredData>(
-  resolve(__dirname, './.storage/equalizer.json')
-)
-const persistentStorage = new PersistentStorage(Date, fileStorage)
-const browserService = new PuppeteerBrowserService({
-  baseUrl: 'https://www.linkedin.com/',
-  cookies: [
-    {
-      name: 'li_at',
-      value: process.env.LI_SESSION_TOKEN,
-      domain: '.www.linkedin.com',
-    },
-  ],
-  runInContainer: process.env.PUPPETEER_RUN_IN_DOCKER === 'true',
-})
+const scheduler = new Scheduler(20000)
 const logger = new EqualizerLogger()
-const dateEvaluator = new DateEvaluator(Date)
-const linkedInService = new LinkedInService(browserService)
 
-;(async () => {
-  const hasOpenAi = OpenAIEvaluator.isKeyValid(process.env.OPENAI_API_KEY)
+scheduler.schedule(async () => {
+  const child = fork(resolve(__dirname + '/src/run.js'))
 
-  if (!(await linkedInService.isUserLoggedIn())) {
+  child.on('exit', (code, signal) => {
+    if (code === 0) {
+      logger.log('Successful running, waiting for the next schedule')
+      return
+    }
     logger.log(
-      "We haven't managed to login to your LinkedIn account with the provided session token. Monitoring hasn't started."
+      `Child process exited with code "${code}" and signal "${signal}"`
     )
-    return
-  }
-
-  if (!hasOpenAi) {
-    logger.log(
-      'OpenAI API key is not valid. We do not evaluate the messages automatically.'
-    )
-  }
-
-  if (
-    !ConfigurationValidator.isEqualizerProfileNameValid(
-      process.env.LI_PROFILE_ID
-    )
-  ) {
-    logger.log(
-      "Provided Equalizer profile name is invalid. Monitoring hasn't started."
-    )
-    return
-  }
-
-  const profileUrl = new ProfileUrl(process.env.LI_PROFILE_ID)
-
-  const isMessageValid =
-    !!process.env.LI_AUTO_MESSAGE &&
-    ConfigurationValidator.isMessageValid(process.env.LI_AUTO_MESSAGE)
-  if (!isMessageValid) {
-    logger.log(
-      'Provided auto message is invalid. We provided a default reply for you.'
-    )
-  }
-  const message = profileUrl.replaceInText(
-    isMessageValid ? process.env.LI_AUTO_MESSAGE : DEFAULT_AUTO_REPLY_TEXT
-  )
-
-  const openAiEvaluator = hasOpenAi
-    ? new OpenAIEvaluator(process.env.OPENAI_API_KEY)
-    : undefined
-  const invitationEvaluator = new InvitationEvaluator(openAiEvaluator)
-  const conversationEvaluator = new ConversationEvaluator(
-    dateEvaluator,
-    openAiEvaluator
-  )
-
-  const autoConnect = new AutoConnect(
-    persistentStorage,
-    linkedInService,
-    invitationEvaluator,
-    conversationEvaluator,
-    logger,
-    message
-  )
-
-  const limiter = new Bottleneck({
-    minTime: 60_000,
-    maxConcurrent: 1,
   })
 
-  scheduleJob('*/10 * * * *', async () => {
-    await limiter.schedule(async () => {
-      await autoConnect.monitorEnquiries()
-    })
-  })
-})()
+  child.on('message', logger.log)
+})
